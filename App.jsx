9654ae063aacc00131ef647f47e1d847916d5522
Adharshini10 <lingampallyadharshini@gmail.com>
2026-06-30 11:27:32 +0530

    Add Employee component to App

--- App.jsx
+++ App.jsx
@@ -1,10 +1,14 @@
 
 import './index.css';  
 import Card from './Card.jsx';
+import Employee from './Emp.jsx';
+
 function App(){
 return(
   <>
 <Card/>
+<Employee name="hello" batch={100} Address="hyd" isemp={false}/>
+<Employee name="hiee" batch={200} Address="japan" isemp={true}/>
   </>
 )
 }
