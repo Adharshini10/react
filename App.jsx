a7626682adbbf37a60700f532e5d00e451145998
Adharshini10 <lingampallyadharshini@gmail.com>
2026-07-04 13:13:52 +0530

    Add Usergreeting component to App

--- App.jsx
+++ App.jsx
@@ -2,13 +2,17 @@
 import './index.css';  
 import Card from './Card.jsx';
 import Employee from './Emp.jsx';
-
+import Usergreeting from './Usergreeting.jsx';
 function App(){
 return(
   <>
 <Card/>
+    
 <Employee name="hello" batch={100} Address="hyd" isemp={false}/>
 <Employee name="hiee" batch={200} Address="japan" isemp={true}/>
+<Employee name=" " batch={0} Address=" " isemp={false}/>
+
+<Usergreeting isloggedin={false} name="pooja" />
   </>
 )
 }
