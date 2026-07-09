12e134c38d8f4376be468d554ed9d131ca57052d
Adharshini10 <lingampallyadharshini@gmail.com>
2026-07-09 22:06:41 +0530

    Enhance List component with additional fruit and rendering
    Added grapes to the fruit list and rendered it as an ordered list.

--- list.jsx
+++ list.jsx
@@ -2,7 +2,10 @@ function List(){
   const fruits=[
     {name:"Apple", no:19},
     {name:"pomegranate",no:1412},
-    {name:"banana",no:124}
+    {name:"banana",no:124},
+    {name:"grapes",no:1}
   ]
+  const list= fruits.map(fruit =><li key={fruit.name}>{fruit.name}: {fruit.no}</li>);
+  return(<ol>{list}</ol>);
 }
 export default List;
