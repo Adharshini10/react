6abb1284fa01745b719f948b092228379ae42f99
Adharshini10 <lingampallyadharshini@gmail.com>
2026-07-12 18:54:51 +0530

    Add sorting options as comments in List component
    Commented out sorting options for the fruits array.

--- list.jsx
+++ list.jsx
@@ -5,7 +5,13 @@ function List(){
     {name:"banana",no:124},
     {name:"grapes",no:1}
   ]
+ // fruits.sort((a,b) => a.name.localeCompare(b.name));//alphabetical order
+//  fruits.sort((a,b) => b.name.localeCompare(a.name));//reverse alphabetical order
+//   fruits.sort((a,b) => a.no-b.no);//numerical order
+//    fruits.sort((a,b) => b.no-a.no);//reverse numerical order
+    
   const list= fruits.map(fruit =><li key={fruit.name}>{fruit.name}: {fruit.no}</li>);
   return(<ol>{list}</ol>);
+
 }
 export default List;
