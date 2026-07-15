5d0aa4735da9725a4c806d88dc953bce679b8983
Adharshini10 <lingampallyadharshini@gmail.com>
2026-07-15 21:36:52 +0530

    Refactor Button component with new click handlers

--- assets/Button.jsx
+++ assets/Button.jsx
@@ -1,16 +1,21 @@
-import { useState } from "react";
-
-function Button() {
-  const [text, setText] = useState("Click me!");
-
-  const handleClick = () => {
-    setText("You clicked me!!");
-    console.log("Button was clicked!");
-  };
-
-  return (
-    <button onClick={handleClick}>{text}</button>
-  );
+function Button(){
+  const butt1= console.log("heyy");
+  const butt2=(name)=> console.log(`${name} you clicked me`);
+  let count=0;
+  const butt3=()=>{
+    if(count<3){
+      count++;
+      console.log(`You clicked me ${count} times!`);
+    }
+    else{
+      console.log("stop clicking");
+    }
+  }
+  return(
+    <div>
+          <button onClick={()=>butt2("minnu")}>BUTTON</button> <br /> <br/>
+          <button onClick={()=>butt3("arshu")}>BUTTON2</button>
+    </div>
+  )
 }
-
 export default Button;
