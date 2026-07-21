71b450b03c24d4d4cc21507430520820e31e283a
Adharshini10 <lingampallyadharshini@gmail.com>
2026-07-21 18:02:58 +0530

    Refactor Button component with improved click handling
    Updated button click handlers and console log messages for clarity.

--- assets/Button.jsx
+++ assets/Button.jsx
@@ -1,20 +1,21 @@
 function Button(){
-  const butt1= console.log("heyy");
-  const butt2=(name)=> console.log(`${name} you clicked me`);
+  const butt1 = console.log("heyy");  //this automatically prints in inspect bcoz butt1 is not a function like butt2
+  const butt2=(name2)=> console.log(`${name} you clicked me`);  //it prints no of times as the button clicked
   let count=0;
   const butt3=()=>{
     if(count<3){
       count++;
-      console.log(`You clicked me ${count} times!`);
+      console.log(`You clicked me ${count} times!`);         //it counts
     }
     else{
-      console.log("stop clicking");
+      console.log(`${name} stop clicking`);         //it prints when the count is upto limit
     }
   }
   return(
     <div>
-          <button onClick={()=>butt2("minnu")}>BUTTON</button> <br /> <br/>
-          <button onClick={()=>butt3("arshu")}>BUTTON2</button>
+      <button onClick={()=>butt1("arshu")}>button</button><br /> <br/>
+          <button onClick={()=>butt2("minnu")}>BUTTON 1</button> <br /> <br/>
+          <button onClick={()=>butt3("arshu")}>COUNT BUTTON</button>
     </div>
   )
 }
