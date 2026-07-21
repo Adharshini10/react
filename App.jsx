16debda38b49bab68c2e9e8406604ced0839cea8
Adharshini10 <lingampallyadharshini@gmail.com>
2026-07-21 19:39:46 +0530

    Refactor App component to include new imports and structure

--- App.jsx
+++ App.jsx
@@ -3,21 +3,32 @@ import './index.css';
 import Card from './Card.jsx';
 import Employee from './Emp.jsx';
 import Usergreeting from './Usergreeting.jsx';
-function App(){
-return(
-  <>
-<Card/>
-    
-<Employee name="hello" batch={100} Address="hyd" isemp={false}/>
-<Employee name="hiee" batch={200} Address="japan" isemp={true}/>
-<Employee name=" " batch={0} Address=" " isemp={false}/>
+import List from './list.jsx';
+import Button from './Button.jsx';
+import Eventhandler from './Eventhandler.jsx';
+import Usestate from './Usestate.jsx';
+function App() {
+  return (
+    <>
+      <Card />
+      <hr />
 
-<Usergreeting isloggedin={false} name="pooja" />
+      <Employee name="hello" batch={100} Address="hyd" isemp={false} />
+      <Employee name="hiee" batch={200} Address="japan" isemp={true} />
+      <Employee name=" " batch={0} Address=" " isemp={false} />
 
-    <button/><br/>
-    <Eventhandler/>
-  
-  </>
-)
+      <Usergreeting isloggedin={false} name="pooja" />
+      <hr />
+
+      <Button />
+      <br />
+      <hr />
+
+      <Eventhandler />
+      <hr />
+      <Usestate/>
+    </>
+  );
 }
+
 export default App;
