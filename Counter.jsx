58b8cb6642ce1920d5115ce0203bc68f77fdeb47
Adharshini10 <lingampallyadharshini@gmail.com>
2026-07-25 08:40:27 +0530

    Add comments for increment and decrement for update function
    Updated the Counter component to include comments explaining how to increment and decrement the counter by multiple values.

--- Counter.jsx
+++ Counter.jsx
@@ -2,9 +2,22 @@ import React,{useState} from "react";
 function Counter(){
 const [counter,setCounter] =useState(0);
 const increment=()=>{
+
+    //  if we want to increment it by 3 like 3,6,9 we should use update function like
+    //  setCounter( counter=counter + 1)
+   // setCounter( counter=counter + 1) (or else we can use 'c' 1st letter of counter)
+   // setCounter( c=c + 1)
+   // using three times it will increment 3 directly
+
     setCounter(counter + 1)
 }
 const decrement=()=>{
+
+    // also in decrement if we want to reduce it for 2 times like 4,2,0.. use
+    //  setCounter(counter=counter -1) //or else use 'c'
+// setCounter( c= c -1)
+//after this ,the state is updating if we use "setCounter(counter -1)" as three times it wont works we should call the object
+// this is called updater function
     setCounter(counter -1)
 }
 const reset=()=>{
