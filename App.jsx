ccf48f862d4af1154eb7fe3579ca6a24cb1bd6e4
Adharshini10 <lingampallyadharshini@gmail.com>
2026-07-26 17:03:32 +0530

    add todo.jsx

--- App.jsx
+++ App.jsx
@@ -10,21 +10,9 @@ import Usestate from './Usestate.jsx';
 import Counter from './Counter.jsx';
 import Onchange from './Onchange.jsx';
 import Colorpicker from './Colorpicker.jsx';
-function App() {
-  return (
-import './index.css';  
-import Card from './Card.jsx';
-import Employee from './Emp.jsx';
-import Usergreeting from './Usergreeting.jsx';
-import List from './list.jsx';
-import Button from './Button.jsx';
-import Eventhandler from './Eventhandler.jsx';
-import Usestate from './Usestate.jsx';
-import Counter from './Counter.jsx';
-import Onchange from './Onchange.jsx';
-import Colorpicker from './Colorpicker.jsx';
 import Listupdate from './Listupdate.jsx';
 import Studentdetails from './Studentdetails.jsx';
+import Todo from './Todo.jsx';
 function App() {
   return (
     <>
@@ -55,36 +43,8 @@ function App() {
       <Listupdate/>
       <hr/>
       <Studentdetails/>
-    </>
-  );
-}
-
-export default App;
-
-    <>
-      <Card />
-      <hr />
-
-      <Employee name="hello" batch={100} Address="hyd" isemp={false} />
-      <Employee name="hiee" batch={200} Address="japan" isemp={true} />
-      <Employee name=" " batch={0} Address=" " isemp={false} />
-
-      <Usergreeting isloggedin={false} name="pooja" />
-      <hr />
-
-      <Button />
-      <br />
-      <hr />
-
-      <Eventhandler />
-      <hr />
-      <Usestate/>
       <hr/>
-      <Counter/>
-      <hr/>
-      <Onchange/>
-      <hr/>
-      <Colorpicker/>
+      <Todo/>
     </>
   );
 }
